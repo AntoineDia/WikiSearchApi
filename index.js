a2a532da2ca7206d6073491b0292c9dd5cbf5604
AntoineDia <diambu.antoine@gmail.com>
2019-02-25 16:51:36 +0100

    Finalisation recherche + animation

--- index.js
+++ index.js
@@ -1,15 +1,54 @@
-document.getElementById('search').addEventListener('keydown', goodcheck);
+//TOINTOIN DIAMBU
 
-function goodcheck(key){
+window.addEventListener('scroll', dessente); 
+function dessente(){
+  if (window.scrollY > 100){
+    document.querySelector('#stick').setAttribute('class','youstick');
+    window.addEventListener('scroll', montee); 
+    window.removeEventListener('scroll', dessente);
+  }
+};
+function montee(){
+  if (window.scrollY < 70){
+    document.querySelector('#stick').removeAttribute('class');
+    window.addEventListener('scroll',dessente);
+    window.removeEventListener('scroll', montee);
+  }
+};
+
+document.querySelector('#stick > span').addEventListener('click', getsearch);
+document.getElementById('search').addEventListener('keydown', isEnter);
+
+function isEnter(key){
   if (key.keyCode === 13) {
-    goodboy();
+    getsearch();
    }
 }
 
-// const url = 'https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&search=terre';
+function getsearch(){
+  if(document.getElementById('search').value != ""){
+    document.querySelector('main').removeChild(document.querySelector('#resultats'));
+    document.querySelector('main').innerHTML += '<div id="resultats"></div>';
+    var url = 'https://en.wikipedia.org/w/api.php?origin=*&action=opensearch&limit=20&search=';
+    url += document.getElementById('search').value;
+    fetch(url).then(function(reponse){
+      return reponse.json();
+    }).then(function(data) {
+      incorpHtml(data);
+    });
+  }
+};
 
-// fetch(url).then(function(resp){
-//   return resp.json();
-// }).then(function(data) {
-//   console.log(data);
-// })
+function incorpHtml(el){
+  var r = document.querySelector('#resultats');
+  for(i = 0; i < el[1].length; i++){
+    r.innerHTML += `
+    <div class="resultat">
+      <h2>${el[1][i]}</h2>
+      <p value="${el[3][i]}">${el[2][i]}</p>
+    </div>
+    `;
+  }
+};
+
+
