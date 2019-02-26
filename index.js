9a9b2434427fa775106908b2cb1a16cdc0f0fe63
AntoineDia <diambu.antoine@gmail.com>
2019-02-26 16:29:24 +0100

    starting iframe integration

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 //TOINTOIN DIAMBU
 
+//  Juste rendre le logo cliqable
+document.querySelector('#searchbar > h1').addEventListener('click', function(){location.reload()});
+
+// {Position: sticky} pour les nuls (genre moi)
 window.addEventListener('scroll', dessente); 
 function dessente(){
-  if (window.scrollY > 100){
+  if (window.scrollY > 110){
+    document.querySelector('#searchbar > h1').setAttribute('class','youpush');
     document.querySelector('#stick').setAttribute('class','youstick');
     window.addEventListener('scroll', montee); 
     window.removeEventListener('scroll', dessente);
@@ -10,21 +15,23 @@ function dessente(){
 };
 function montee(){
   if (window.scrollY < 70){
+    document.querySelector('#searchbar > h1').removeAttribute('class');
     document.querySelector('#stick').removeAttribute('class');
     window.addEventListener('scroll',dessente);
     window.removeEventListener('scroll', montee);
   }
 };
 
+//Ajoute l'événement de recherche
 document.querySelector('#stick > span').addEventListener('click', getsearch);
 document.getElementById('search').addEventListener('keydown', isEnter);
-
 function isEnter(key){
   if (key.keyCode === 13) {
     getsearch();
    }
 }
 
+//  Récupère les données
 function getsearch(){
   if(document.getElementById('search').value != ""){
     document.querySelector('main').removeChild(document.querySelector('#resultats'));
@@ -36,19 +43,52 @@ function getsearch(){
     }).then(function(data) {
       incorpHtml(data);
     });
+  } else {
+    location.reload();
   }
 };
 
+//Affiche les données dans l'html
+var liens = [];
 function incorpHtml(el){
   var r = document.querySelector('#resultats');
+  if(el[1].length == 0){
+    el[1].push('Nothing here :(');
+    el[2].push('Search something that exist<br>THIS IS A 404!! ABORT!!');
+    el[3].push('');
+  };
   for(i = 0; i < el[1].length; i++){
-    r.innerHTML += `
-    <div class="resultat">
-      <h2>${el[1][i]}</h2>
-      <p value="${el[3][i]}">${el[2][i]}</p>
-    </div>
-    `;
-  }
+    let div = document.createElement('div');
+    div.setAttribute('class','resultat');
+    div.setAttribute('id',`resu${i}`);
+    div.setAttribute('url',el[3][i])
+    let h2 = document.createElement('h2');
+    h2.innerText = el[1][i];
+    let p = document.createElement('p');
+    p.innerText = el[2][i];
+    let iframe = document.createElement('iframe');
+    iframe.setAttribute('class','myiframe')
+    div.appendChild(h2);
+    div.appendChild(p);
+    div.appendChild(iframe);
+    r.appendChild(div);
+    document.getElementById(`resu${i}`).addEventListener("click",hello);
+  };
 };
 
+function hello(divbrut){
+  var div = "";
+  if(divbrut.path.length<8){
+    div = divbrut.path[0];
+  } else {
+    div = divbrut.path[1];
+  }
+  ifr = div.getElementsByTagName('iframe')[0];
+  ifr.setAttribute('class','newframe');
+  ifr.setAttribute('src',div.getAttribute('url'));
+}
 
+// let url = 'https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=revisions&rvprop=content&format=json&titles=';
+//   page = page.replace('https://en.wikipedia.org/wiki/','');
+//   page = page.replace(/[\_]+/g,'%20');
+//   url+=page;
