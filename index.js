ef44de7ee00bd45dc900677cb656403bbcdb2155
AntoineDia <diambu.antoine@gmail.com>
2019-02-27 10:17:39 +0100

    end of colapsing iframe

--- index.js
+++ index.js
@@ -4,12 +4,12 @@
 document.querySelector('#searchbar > h1').addEventListener('click', function(){location.reload()});
 
 // {Position: sticky} pour les nuls (genre moi)
-window.addEventListener('scroll', dessente); 
+window.addEventListener('scroll', dessente);
 function dessente(){
   if (window.scrollY > 110){
     document.querySelector('#searchbar > h1').setAttribute('class','youpush');
     document.querySelector('#stick').setAttribute('class','youstick');
-    window.addEventListener('scroll', montee); 
+    window.addEventListener('scroll', montee);
     window.removeEventListener('scroll', dessente);
   }
 };
@@ -49,35 +49,42 @@ function getsearch(){
 };
 
 //Affiche les données dans l'html
-var liens = [];
 function incorpHtml(el){
   var r = document.querySelector('#resultats');
+
   if(el[1].length == 0){
     el[1].push('Nothing here :(');
     el[2].push('Search something that exist<br>THIS IS A 404!! ABORT!!');
     el[3].push('');
   };
+
   for(i = 0; i < el[1].length; i++){
+
     let div = document.createElement('div');
     div.setAttribute('class','resultat');
     div.setAttribute('id',`resu${i}`);
-    div.setAttribute('url',el[3][i])
+    div.setAttribute('url',el[3][i]);
+
     let h2 = document.createElement('h2');
     h2.innerText = el[1][i];
+
     let p = document.createElement('p');
     p.innerText = el[2][i];
+
     let iframe = document.createElement('iframe');
-    iframe.setAttribute('class','myiframe')
+    iframe.setAttribute('class','myiframe');
+
     div.appendChild(h2);
     div.appendChild(p);
     div.appendChild(iframe);
+
     r.appendChild(div);
-    document.getElementById(`resu${i}`).addEventListener("click",hello);
+    document.getElementById(`resu${i}`).addEventListener('click',hellowiki);
   };
 };
 
-function hello(divbrut){
-  var div = "";
+function hellowiki(divbrut){
+  var div = '';
   if(divbrut.path.length<8){
     div = divbrut.path[0];
   } else {
@@ -86,6 +93,30 @@ function hello(divbrut){
   ifr = div.getElementsByTagName('iframe')[0];
   ifr.setAttribute('class','newframe');
   ifr.setAttribute('src',div.getAttribute('url'));
+  div.removeEventListener('click',hellowiki);
+  div.addEventListener('click', byewiki);
+  if(document.getElementById('needcol')){
+    console.log('hey');
+    el = document.getElementById('needcol');
+    el.setAttribute('id','');
+    byewiki(el);
+  }
+  div.setAttribute('id','needcol');
+}
+
+function byewiki(elcol){
+  if(elcol.target){
+    if(elcol.path.length<8){
+      elcol = elcol.path[0];
+    } else {
+      elcol = elcol.path[1];
+    }
+  }
+  elcol.removeEventListener('click',byewiki);
+  elcol.addEventListener('click', hellowiki);
+  elcol.setAttribute('id','');
+  let ifr = elcol.getElementsByTagName('iframe')[0];
+  ifr.setAttribute('class','byeframe');
 }
 
 // let url = 'https://en.wikipedia.org/w/api.php?origin=*&action=query&prop=revisions&rvprop=content&format=json&titles=';
